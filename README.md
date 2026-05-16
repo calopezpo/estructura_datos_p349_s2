bfa7f69a0795c17968c931f1691c71bc6d705ef5
carlos <calopezpo@puce.edu.ec>
2026-05-16 12:28:35 -0500

    tarea  terminada ;)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # estructura_datos_p349_s2
+Pre-requisitos:
+se necesita python 3.3
+:)
---
+++ matrix 1al 1000000.py
@@ -0,0 +1,45 @@
+matriz = list(range(1, 1000000))
+numero = int(input("Ingrese el numero que desea buscar: "))
+
+
+pasos_secuencial = 0
+
+for i in range(len(matriz)):
+    pasos_secuencial += 1
+
+    if matriz[i] == numero:
+        print("="*50)
+        print("BUSQUEDA SECUENCIAL")
+        print("Número encontrado con búsqueda secuencial", numero)
+        print("Pasos:", pasos_secuencial)
+        print("="*50)
+
+        break
+
+izquierda = 0
+derecha = len(matriz) - 1
+
+pasos_binaria = 0
+
+while izquierda <= derecha:
+
+    pasos_binaria += 1
+
+    medio = (izquierda + derecha) // 2
+
+    if matriz[medio] == numero:
+
+        print("="*50)
+        print("BUSQUEDA BINARIA")
+        print("Número encontrado:", numero)
+        print("Pasos realizados:", pasos_binaria)
+        print("="*50)
+        break
+
+    elif matriz[medio] < numero:
+
+        izquierda = medio + 1
+
+    else:
+
+        derecha = medio - 1
